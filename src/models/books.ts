fbbf040ef333c0caabddd679ca38b88f61a143f1
Islam Sulaiman <ma.eslam@yahoo.com>
2022-02-27 10:15:29 +0300

    adding place holders for model methods to protect against injection attacks

--- src/models/books.ts
+++ src/models/books.ts
@@ -18,6 +18,7 @@ export type Book = {
 //create the sql class which will contain all the CRUD sql methods
 export class Book_handlers {
     //create method that read all the rows of DB
+    //the promise type array of Book because we want to get all the rows from books table, which will return multiple elements of strings
     async index () : Promise<Book[]> {
         try {
             const conn = await client.connect();
@@ -30,34 +31,43 @@ export class Book_handlers {
         }
     }
     // create delete sql to delete a row based on id;
-    async delete (id : number) : Promise<void>{
+    async delete (id : number) : Promise<Book>{
         try {
             const conn = await client.connect();
-            const sql = `DELETE FROM books WHERE id = ${id}`;
-            const result = await conn.query(sql);
-            conn.release();            
+            const sql = `DELETE FROM books WHERE id = ($1) RETURNING *`;
+            const result = await conn.query(sql, [id]);
+            const book = result.rows[0];
+            conn.release();
+            return book;            
         } catch (error) {
             throw new Error (`The error is ${error}`)
         }
     }
     // create insert sql to insert a row to the table
-    async insert ( title : string,total_pages : number,author : string,type : string,summary : string): Promise<void>{
+    // 'b' is an object containing all the query data
+    async insert ( b : Book ): Promise<Book>{
         try {
             const conn = await client.connect();
-            const sql = `INSERT INTO books (title ,total_pages ,author ,type,summary) VALUES (${title}, ${total_pages}, ${author}, ${type}, ${summary})`;
-            const result = await conn.query(sql);
-            conn.release();    
+            // add $1, $2 ... as a place holder for the actual values to insert 
+            //'RETURNING *' this clause will final line that will be inserted to the DB so we could check what is the final line added, we could use it with DELETE and update too.
+            const sql = `INSERT INTO books (title ,total_pages ,author , type, summary) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+            //add sql as a 1st arg , then add an array of the target values from the object Book.
+            const result = await conn.query(sql, [b.title, b.total_pages, b.author, b.type, b.summary]);
+            //save the 1st element from the array returned from "result" variable. 
+            const book = result.rows[0];
+            conn.release();   
+            return book; 
             
         } catch (error) { 
             throw new Error (`The error is ${error}`)
         }
     }
-    //create a methos to show a row based on id
+    //create a methods to show a row based on id
     async show (id : number) : Promise<Book>{
         try {
             const conn = await client.connect();
-            // here I'am placing the value with '?' to defete againest injection attacks
-            const sql = `SELECT * FROM books WHERE id = ?`;
+            // here I'am placing the value with ($1, $2, $3 ....) to delete against injection attacks
+            const sql = `SELECT * FROM books WHERE id = ($1)`;
             // I'am adding a second argument here to substitute the '?' from the sql query up
             const result = await conn.query(sql, [id]);
             conn.release(); 
@@ -68,6 +78,4 @@ export class Book_handlers {
             throw new Error (`The error is ${error}`)
         }
     }
-
-
 }
