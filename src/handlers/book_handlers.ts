dabb1b374224e6039675bd0f9291b3a05589adac
Islam Sulaiman <ma.eslam@yahoo.com>
2022-03-02 08:21:11 +0300

    creating handlers for the SQL methods

---
+++ src/handlers/book_handlers.ts
@@ -0,0 +1,60 @@
+//import SQL methods class to destructure it later and the type
+import {Book, Book_handlers } from "../models/books";
+
+//import express;
+import  express, {Request, Response}  from "express";
+
+
+//create an instance of Book_handlers class
+let book = new Book_handlers();
+
+
+//create a route for index() method;
+//this route takes an argument id
+const index = async (_req: Request, res: Response)=>{   //add _ before req, because we are not going to use it here.
+    try {
+        const result = await book.index();
+        res.json(result);
+    } catch (error) {
+        console.log(`Error from INDEX ${error}`)
+    }
+}
+
+//create a route for show() method;
+//this route takes an argument id
+const show = async (req: Request, res: Response)=>{
+    try {
+        const result = await book.show(req.body.id)
+        res.json(result);
+    } catch (error) {
+        console.log(`Error from SHOW ${error}`)
+    }
+}
+
+//create a route for delete() method;
+//this route takes an argument id
+const destroy = async (req :Request , res :Response)=>{
+    try {
+        const result = await book.delete(req.body.id);
+        res.json(result);
+    } catch (error) {
+        console.log(`Error from DELETE ${error}`)
+    }
+
+}
+
+//create a route for create() method;
+//this route takes an argument id
+const create = async (req: Request, res: Response) => {
+    try {
+        const result = await book.create(req.body.id);
+        res.json(result);
+    } catch (error) {
+        console.log(`Error from CREATE ${error}`)
+    }
+}
+
+export const index_route = (app : express.Application)=>{
+    app.get('/book', index);
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
+//import route handlers
+import { index_route } from './handlers/book_handlers';
+
 const app: express.Application = express();
 
 //dont add type for this variable because it's already initialized with string value and typescript can
@@ -11,6 +14,8 @@ app.use(bodyParser.json());
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World!');
 });
+
+index_route(app);
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
