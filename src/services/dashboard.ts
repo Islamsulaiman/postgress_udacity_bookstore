a25f8492442bf2bd27e10537bf47ca59ef6ef31d
Islam Sulaiman <ma.eslam@yahoo.com>
2022-03-07 15:12:57 +0300

    creating services folder with dashboard file

---
+++ src/services/dashboard.ts
@@ -0,0 +1,10 @@
+//this file will contain the services methods that doesn't have direct functionality with CRUD operation
+
+import client from "../database";
+
+export class serviceMethods{
+    //create dashboard to view the name ,price and order_id from products and order_products table.
+    async dashboard() :Promise<{}>{
+
+    }
+}
