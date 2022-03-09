8c08340d50028c35eafa7241aa8a5bee6787e1ea
islam Sulaiman <ma.eslam@yahoo.com>
2022-03-10 00:24:29 +0300

    specReporter

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,22 +1,48 @@
-import {
-  DisplayProcessor,
-  SpecReporter,
-  StacktraceOption,
-} from 'jasmine-spec-reporter';
-import SuiteInfo = jasmine.SuiteInfo;
+// import {
+//   DisplayProcessor,
+//   SpecReporter,
+//   StacktraceOption,
+// } from 'jasmine-spec-reporter';
+// import SuiteInfo = jasmine.SuiteInfo;
 
-class CustomProcessor extends DisplayProcessor {
-  public displayJasmineStarted(info: SuiteInfo, log: string): string {
-    return `${log}`;
-  }
-}
+// class CustomProcessor extends DisplayProcessor {
+//   public displayJasmineStarted(info: SuiteInfo, log: string): string {
+//     return `${log}`;
+//   }
+// }
 
+// jasmine.getEnv().clearReporters();
+// jasmine.getEnv().addReporter(
+//   new SpecReporter({
+//     spec: {
+//       displayStacktrace: StacktraceOption.NONE,
+//     },
+//     customProcessors: [CustomProcessor],
+//   })
+// );
+
+
+import { SpecReporter } from 'jasmine-spec-reporter';
+import dotenv from 'dotenv';
+dotenv.config();
+process.env.ENV = 'test';
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(
   new SpecReporter({
-    spec: {
-      displayStacktrace: StacktraceOption.NONE,
+    suite: {
+      displayNumber: true
     },
-    customProcessors: [CustomProcessor],
+    spec: {
+      displayPending: false,
+      displayDuration: false,
+      displayErrorMessages: true
+    },
+    summary: {
+      displayDuration: true,
+      displayFailed: true,
+      displaySuccessful: false,
+      displayPending: true,
+      displayErrorMessages: false
+    }
   })
 );
