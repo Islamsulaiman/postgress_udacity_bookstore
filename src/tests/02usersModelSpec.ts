a7a65e376b7d6340e087ae25cf689515fefc8269
islam Sulaiman <ma.eslam@yahoo.com>
2022-03-13 23:03:55 +0300

    create and show one user tests

--- src/tests/02usersModelSpec.ts
+++ src/tests/02usersModelSpec.ts
@@ -40,31 +40,47 @@ describe('Test that every model inside usersModel is defined', () => {
   });
 });
 
-describe('test usersMethods connection status', () => {
-  beforeAll(() => console.log('hello'));
+//in this suite will test user behavior models.
+describe('test create user logic', () => {
 
-  it('index', async () => {
-    const response = await requestUserModels.get('/showAllUsers');
-    expect(response.status).toEqual(200);
-  });
-  it('createUser', async () => {
-    const response = await requestUserModels.get('/createUser');
-    expect(response.status).toEqual(200);
-  });
-  //   it("showOneUser", async ()=>{
-  //   const response = await requestUserModels.get("/showOneUser/1");
-  //   expect(response.status).toEqual(200);
-  // })
-  //   it("index", async ()=>{
-  //   const response = await requestUserModels.get("/showAllUsers");
-  //   expect(response.status).toEqual(200);
-  // })
-  //   it("index", async ()=>{
-  //   const response = await requestUserModels.get("/showAllUsers");
-  //   expect(response.status).toEqual(200);
-  // })
-  //   it("index", async ()=>{
-  //   const response = await requestUserModels.get("/showAllUsers");
-  //   expect(response.status).toEqual(200);
-  // })
+  //create user object that we will pass to create method like the body we send via postman, should match the real model.
+  const userObject: Users = {
+    f_name : "f_name test",
+    l_name: "l_name test",
+    user_name: "user_name test",
+    password: "test pass",
+    age: 20,
+  }
+
+  //this will run before all the tests, we should create our first user in this step so we can find someone to test on later, and this insures that we have user ready because the following tests order could change because it asynchronous functions. 
+  beforeAll(async() =>{
+    //using create method and the object we created as an argument for create model which takes user info as an object.
+    const newUser = await user.create(userObject);
+  }) 
+
+  //this will help us delete the table and make it ready for the rest of tests inside this file and others because they are all conducted on the same DB and tables 
+  afterAll(async()=>{
+    const conn = await client.connect();
+
+    //delete all created users inside users table to be fresh for other tests, 
+    const sqlDELETE = "DELETE FROM users;";
+
+    // after deleting all users, the id sequence would't start from 1 because when rows deleted the id sequence keep counting on, but we need in other test's after this one the sequence to start from 1 again, so we should alter the sequence like this.
+    const sqlAlterTable = "ALTER SEQUENCE users_id_seq RESTART WITH 1;";
+    
+    //we need now to perform the previous two SQL queries.
+    const Alter = await conn.query(sqlAlterTable);
+    const Delete = await conn.query(sqlDELETE);
+
+    conn.release();
+  })
+
+  it("user should be created under id 1",async ()=>{
+
+    //now we are testing show() model that returns one user with id no.1 , that's why we altered the sequence because we depend on it in our tests
+    const showOneUser = await user.show(1)
+    //AFTER THE USER we created BEEN returned, we can expect the values we entered to be true like age. 
+    expect(showOneUser.age).toBe(20);
+  })
+
 });
